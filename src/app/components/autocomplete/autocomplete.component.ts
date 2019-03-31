0c8326d83bb7972be7873cab605dd9b9491bf3c9
yanivfi <fishman.yaniv@gmail.com>
2019-03-31 14:30:29 +0300

    1 file modified

--- src/app/components/autocomplete/autocomplete.component.ts
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'edu-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.css']
 })
-export class EduAutocompleteComponent implements OnInit {
+export class EduAutocompleteComponent implements OnInit, AfterViewInit {
 
   color: string;
 
@@ -19,7 +19,17 @@ export class EduAutocompleteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+      //document.getElementById("UrlBar").style.display = "none";
+     /* const els = self.iframeImplementation.nativeElement.contentDocument.getElementsByClassName('header');
+      if (els.length > 0) {
+        els[0].style.display = 'none';
+      }*/
+    
   }
-  
+  ngAfterViewInit() {
+      //document.getElementById("UrlBar").style.display = "none";
+
+  }
 
 }
