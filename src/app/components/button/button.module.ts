0e3465178dd3d53092b6932c57c515c37dfb16cc
yanivfi <fishman.yaniv@gmail.com>
2019-04-01 14:04:55 +0300

    1 file modified

--- src/app/components/button/button.module.ts
+++ src/app/components/button/button.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { EduButtonComponent } from './button.component'
 import { EduButtonRoutingModule } from './button-routing.module';
 
 import { ButtonModule } from 'primeng/button';
 
+const routes: Routes = [
+  { path: '', component: EduButtonComponent }
+];
+
 @NgModule({
-    imports: [ FormsModule, ButtonModule, EduButtonRoutingModule ],
+    imports: [ 
+      FormsModule, 
+      ButtonModule, 
+      RouterModule.forChild(routes),
+    ],
     exports: [ EduButtonComponent ],
     declarations: [ EduButtonComponent ],
     entryComponents: [ EduButtonComponent ]
